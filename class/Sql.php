460614210df9412c02d9b1fc43c86015b8b2d50c
Pedro <pedrohsmachado77@gmail.com>
2018-05-30 17:13:54 -0300

    iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,48 @@
+<?php
+ // classe Sql extende do PDO que é nativo do php ( bind param, execute, prepare, etc)
+class Sql extends PDO {
+
+  private $conn;
+
+  // quando alguem chamar a classe new Sql, o construtor conectará
+  // automaticamente no banco de dados
+  public function __construct() {
+    $this->conn = new PDO("mysql:host=localhost;dbname=pedroits","root","");
+  }
+
+  // funcao para setar parametros no db
+  private function setParams($statment, $parameters = array()){
+    // Associando os parâmetros
+    foreach ($parameters as $key => $value) {
+        $this->setParam($key, $value);
+    }
+  }
+
+  private function setParam($statment, $key, $value) {
+    $statment->bindParam($key, $value);
+  }
+
+  // funcao query executa um comando no banco de dados
+  // rawQuery = nosso comando SQL
+  public function query($rawQuery, $params = array())
+  {
+      $stmt = $this->conn->prepare($rawQuery);
+
+      $this->setParams($stmt, $params);
+
+      $stmt->execute();
+
+      return $stmt;
+  }
+
+  public function select($rawQuery, $params=array()):array {
+    $stmt = $this->query($rawQuery, $params);
+    // FETCH ASSOC PEGA SOMENTE OS DADOS ASSOCIATIVOS SEM OS INDEX
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+
+}
+
+
+?>
